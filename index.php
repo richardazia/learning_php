a5ffd8d8701751508910e6b475430ecedf9da902
Richard Azia <richard@main-vision.com>
2022-05-31 17:47:26 +0200

    The php basics

--- index.php
+++ index.php
@@ -1,29 +1,70 @@
-<!-- <?php
-  // This is a comment
-  echo 'Hello World';
-?>
-
-<?='Hello World'?>
 
 <?php
-  print 'hello world'
-?>
-/*
-  Same convention as for other languages
-*/
+  $name = 'Richard';
+  echo "Hello, $name. ";
+  // concatenation
+  echo 'Hello, ' .$name . '!';
 
-// Using variables -->
+  echo 'Welcome to Richard\'s page';
 
-<?php
+  echo "This is line 1 \n";
+  echo "This is line 2 \n";
 
-  $age = 34;
-  $name = DuckWorth:
+  echo 'Richard\'s nickname in this place was unknown!';
 
-  echo $age;
+  //Indexable array
 
-  echo $name;
+  $colours = array('Red', 'Green', 'Blue');
+  print_r( $colours );
+  echo $colours[2];
 
-  $doube_age = $age * 2;
-  echo $double_age;
+  echo '<h1>' . $colours[1] . '</h1>';
 
-?>
+  $xolours[] = 'orange';
+  print_r( $colours );
+
+  // Playing with asssociative arrays
+  $walk_villages = array(
+    'lakeside' => 'Coppet, Vaud',
+    'Jura' => 'St Cergue, Vaud',
+    'small' => 'Tranchepied, Vaud',
+  );
+
+  echo '<pre';
+  print_r( $walk_villages );
+  echo '</pre>';
+
+  echo '<p>' . $walk_villages['small'] . '</p>';
+
+  //Multidimentional Array
+  $people = array(
+    'Joe' => array(
+      'age' => 34,
+      'job' => 'teacher',
+      'state' => 'PA',
+    ),
+    'Phil' => array(
+      'age' => 19,
+      'job' => 'Photographer',
+      'state' => 'NY',
+    ),
+    'Mike' => array(
+      'age' => 25,
+      'job' => 'logistician',
+      'state' => 'NY',
+    ),
+    'Rob' => array(
+      'age' => 27,
+      'job' => 'sheep herder',
+      'state' => 'Wy',
+    ),
+  );
+
+  echo '<pre>';
+  print_r( $people );
+  echo '</pre>';
+
+  //To get data
+  echo '<h3>'. $people['Mike']['job'] . '</h3>';
+
+ ?>
