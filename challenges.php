4f3073a9294e15f4468631cc1aea2632b65e50b0
Richard Azia <richard@main-vision.com>
2022-05-31 17:46:12 +0200

    first php challenge

---
+++ challenges.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Teresa"; // Heading 1
+$city = "Geneva";
+$film = "The Unbearable Lightness of Being"; //Italics
+$friend = "Jean-Christophe"; // Bold
+$sweets = "Mentos";
+
+echo "<h1>Dear $name!</h1>\n\n";
+
+echo "<p>Today I went to $city. to watch <i>$film</i> with <b>$friend</b> where we shared $sweets . </p>";
+
+echo "<p>Isn't this pandemic a nuissance, I would hacve preferred to sit at home than risk covimonkeypoxvid. </p>";
+ ?>
