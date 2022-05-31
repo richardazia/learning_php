14a4567140380f4b01bb1c0e8753d1c0de06c677
Richard Azia <richard@main-vision.com>
2022-05-31 16:03:19 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- <?php
+  // This is a comment
+  echo 'Hello World';
+?>
+
+<?='Hello World'?>
+
+<?php
+  print 'hello world'
+?>
+/*
+  Same convention as for other languages
+*/
+
+// Using variables -->
+
+<?php
+
+  $age = 34;
+  $name = DuckWorth:
+
+  echo $age;
+
+  echo $name;
+
+  $doube_age = $age * 2;
+  echo $double_age;
+
+?>
